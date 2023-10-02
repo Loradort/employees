77b42548c461acc12864aa1745ed7345ceedec4c
Loradort <stu6512732107@sskru.ac.th>
2023-10-02 19:46:20 +0700

    meow

--- room/wsgi.py
+++ room/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'room.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resgis.settings')
 
 application = get_wsgi_application()
+
+app = get_wsgi_application()
